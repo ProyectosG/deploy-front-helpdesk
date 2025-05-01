962019e20662c1514b0dfe761386c5ef8e6b3d40
ProyectosG <proyectosgithub2025@gmail.com>
2025-05-01 21:11:10 +0000

    secret04

--- src/Components/Users/ViewerUsers.jsx
+++ src/Components/Users/ViewerUsers.jsx
@@ -53,9 +53,9 @@ function ViewerUsers() {
   
   return (
     <Container className = "container-fluid py-5">
-      {<Link to='/dashboard/newuser'> */}
+      <Link to='/dashboard/newuser'>
         <button>Add New User</button>  
-      </Link> */}
+      </Link> 
       
         <Table striped bordered hover size="sm" >    
           <thead className = "headerT">
